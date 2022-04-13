d5b4cb7ba442bb7baddf646ca55820688ece45c4
trathor1 <trathor1@its.jnj.com>
2022-04-13 18:51:08 +0530

    added

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -17,10 +17,10 @@ function Landing() {
             job <span>tracking</span> app
           </h1>
           <p>
-            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-            narwhal.
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
           </p>
           <Link to="/register" className="btn btn-hero">
             Login/Register
